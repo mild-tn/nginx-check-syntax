da27c797f76021ab96a36e2eeec49d3a183fe660
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2020-05-14 13:06:55 +0700

    Remove import core

--- nginx_syntax/nginx_syntax/urls.py
+++ nginx_syntax/nginx_syntax/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import CoreView
 
 
 from rest_framework.routers import DefaultRouter
