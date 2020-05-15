a57854f13c0320595cbd5e68f71ef24068a35156
SyndromeProject <infra@prontomarketing.com>
2020-05-15 13:40:42 +0700

    Create nginx genarate file function

--- nginx_syntax/redirection/utils.py
+++ nginx_syntax/redirection/utils.py
@@ -1,7 +1,18 @@
 import subprocess
+import os
 
-def check_nginx_syntax():
-    command = ["docker", "run", "--rm", "-t", "-a", "stdout", "--name", "my-nginx", "-v", "hello.conf:/etc/nginx/:ro", "nginx:latest", "nginx", "-c", "/etc/nginx/nginx.conf", "-t"]
+dirname = os.path.dirname(__file__)
+
+def check_nginx_syntax(path_dir):
+    container_path = ':/etc/nginx/conf.d/default.conf:ro'
+    nginx_volume = path_dir + container_path
+    command = ["docker", "run", "--rm", "-t", "-a", "stdout", "--name", "my-nginx", "-v", nginx_volume, "nginx:latest", "nginx", "-c", "/etc/nginx/nginx.conf", "-t"]
     result = subprocess.run(command)
     return result
 
+def create_nginx_file(domain, rule, redirection_to, is_permanent, is_active, is_http_to_https):
+    file_name = domain + '.conf'
+    path_dir = os.path.join(dirname, '../../nginx-redirect-config/' + file_name)
+    create_nginx = open(path_dir, 'x')
+    nginx_check_syntax = check_nginx_syntax(path_dir)
+    print (nginx_check_syntax.returncode)
--- nginx_syntax/redirection/views.py
+++ nginx_syntax/redirection/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Redirection
-from .utils import check_nginx_syntax
+from .utils import check_nginx_syntax, create_nginx_file
 
 from .serializers import RedirectionSerializer
 
@@ -11,8 +11,8 @@ class RedirectionListAPI(APIView):
     def get(self, request):
         redirection = Redirection.objects.all()
         serializer = RedirectionSerializer(redirection, many=True)
-        result = check_nginx_syntax()
-        print(result.returncode)
+        result = create_nginx_file('mild.test', '/', '/test', True, False, True)
+        print(result)
         print('=====2'*10)
         return  Response(serializer.data)
 
